99036d955bb3c4347943ad55713bb601a9692b7c
1988db <elozrana@gmail.com>
2022-05-18 22:21:16 +0200

    fix: add document fragment - one step appending method

--- index.js
+++ index.js
@@ -1,54 +1,53 @@
 document.addEventListener('DOMContentLoaded', ()=> {
     //header section
         //logo
-    const newHeader = document.createElement('header');
-    const main = document.querySelector('main');
-    main.appendChild(newHeader);
-    const newLogoWrapper = document.createElement('div');
-    const header = main.children[0];
-    header.appendChild(newLogoWrapper);    
-    const newH1 = document.createElement('h1');
-    newH1.classList.add('logo');
-    newH1.innerText = 'Books shop';
-    const newPLogo = document.createElement('p');
-    newPLogo.classList.add('logo-sub');
-    newPLogo.innerText = 'The best on-line book shop';
-    const logoWrapper = header.children[0];
-    logoWrapper.appendChild(newH1);
-    logoWrapper.appendChild(newPLogo);
+    const header = document.createElement('header');    
+    const logoWrapper = document.createElement('div');
+    header.appendChild(logoWrapper);       
+    const h1 = document.createElement('h1');
+    h1.classList.add('logo');
+    h1.textContent = 'Books shop';
+    const pLogo = document.createElement('p');
+    pLogo.classList.add('logo-sub');
+    pLogo.innerText = 'The best on-line book shop';  
+    logoWrapper.appendChild(h1);
+    logoWrapper.appendChild(pLogo);
         //nav
-    const newNavWrapper = document.createElement('div');
-    newNavWrapper.classList.add('nav-wrapper');    
-    header.appendChild(newNavWrapper);    
-    const newNav = document.createElement('nav');
-    const navWrapper = header.children[1];
-    navWrapper.appendChild(newNav);    
-    const newUl = document.createElement('ul');
-    newUl.classList.add('menu-li');
-    const nav = navWrapper.children[0];
-    nav.appendChild(newUl);    
-    const newLiBooks = document.createElement('li');
-    newLiBooks.innerText = 'Books';
-    const newLiContacts = document.createElement('li');
-    newLiContacts.innerText = 'Contacts';
-    const newLiShoppingCart = document.createElement('li');
-    newLiShoppingCart.innerText = 'Shopping cart';
-    const ul = nav.children[0];
-    ul.appendChild(newLiBooks);
-    ul.appendChild(newLiContacts);
-    ul.appendChild(newLiShoppingCart);
-    const newShoppingCartWrapper = document.createElement('div');
-    newShoppingCartWrapper.classList.add('shopping-cart-wrapper');
-    navWrapper.appendChild(newShoppingCartWrapper);
-    const newBooksCount = document.createElement('div');
-    newBooksCount.classList.add('books-count');
-    newBooksCount.innerText = '0';
-    const shoppingCartWrapper = navWrapper.children[1];
-    shoppingCartWrapper.appendChild(newBooksCount);
+    const navWrapper = document.createElement('div');
+    navWrapper.classList.add('nav-wrapper');    
+    header.appendChild(navWrapper);    
+    const nav = document.createElement('nav');    
+    navWrapper.appendChild(nav);    
+    const ul = document.createElement('ul');
+    ul.classList.add('menu-li');    
+    nav.appendChild(ul);    
+    const liBooks = document.createElement('li');
+    liBooks.textContent = 'Books';
+    const liContacts = document.createElement('li');
+    liContacts.textContent = 'Contacts';
+    const liShoppingCart = document.createElement('li');
+    liShoppingCart.textContent = 'Shopping cart';    
+    ul.appendChild(liBooks);
+    ul.appendChild(liContacts);
+    ul.appendChild(liShoppingCart);
+    const shoppingCartWrapper = document.createElement('div');
+    shoppingCartWrapper.classList.add('shopping-cart-wrapper');
+    navWrapper.appendChild(shoppingCartWrapper);
+    const booksCount = document.createElement('div');
+    booksCount.classList.add('books-count');
+    booksCount.textContent = '0';    
+    shoppingCartWrapper.appendChild(booksCount);
     
+    const fragment = new DocumentFragment();
+    fragment.appendChild(header);
+    const main = document.querySelector('main');
+    main.appendChild(fragment);
+
     //display section
     const newDisplaySection = document.createElement('section');
     newDisplaySection.classList.add('display');
+
+    //fetch data from json file
 
     const displayFunction = (object) => {
         
