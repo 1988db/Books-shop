c50cf160d2fc5a9a5a1ac0097cb0730c7e2c6f83
1988db <elozrana@gmail.com>
2022-05-19 11:55:29 +0200

    feat: add addProducts function

--- index.js
+++ index.js
@@ -45,6 +45,18 @@ document.addEventListener('DOMContentLoaded', ()=> {
     //display section
     const displaySection = document.createElement('section');
     displaySection.classList.add('display');
+    //products
+    const products = document.createElement('div');
+    products.classList.add('products');
+    displaySection.appendChild(products);
+    const productsH2 = document.createElement('h2');
+    products.classList.add('our-books');
+    productsH2.textContent = 'Our books';
+    products.appendChild(productsH2);
+    const productsContainer = document.createElement('div');
+    productsContainer.classList.add('products-container');
+
+    
     main.appendChild(displaySection);
 
     //fetch data from json file
@@ -59,11 +71,53 @@ document.addEventListener('DOMContentLoaded', ()=> {
         });
     
 
-    console.log(booksList);
-    //append page structure to the main section
+    //create display section content
+    
+    //append page structure to DOM
     const fragment = new DocumentFragment();
     fragment.appendChild(main);
     
     const wrapper = document.querySelector('.wrapper');
     wrapper.appendChild(fragment);
+
+    //function draw product cart
+    const drawProductCart = (object) => {
+        const productCard = document.createElement(div);
+        productCard.classList.add('product-card');
+        const productImg = document.createElement('img')
+        productImg.classList.add('product-img');
+        productImg.setAttribute('href', object.imageLink);
+        productCard.appendChild(productImg);
+        const author = document.createElement('h4');
+        author.classList.add('author');
+        author.textContent = object.author;
+        productCard.appendChild(author);
+        const title = document.createElement('h3');
+        title.classList.add('title');
+        title.textContent = object.title;
+        productCard.appendChild(title);
+        const price = document.createElement('h4');
+        price.classList.add('price');
+        price.textContent = object.price;
+        productCard.appendChild(price);
+        const cardButtonsWrapper = document.createElement('div');
+        cardButtonsWrapper.classList.add('card-buttons-wrapper');
+        const cardButton1 = document.createElement('div');
+        const cardButton2 = document.createElement('div');
+        cardButton1.classList.add('card-button show-more');
+        cardButton2.classList.add('card-button add-to-cart');
+        cardButton1.textContent = 'Show more';
+        cardButton2.textContent = 'Add to cart';
+        cardButtonsWrapper.appendChild(cardButton1);
+        cardButtonsWrapper.appendChild(cardButton2);
+        productCard.appendChild(cardButtonsWrapper);
+        productsContainer.appendChild(productCard);
+    };
+
+    //add and draw products
+    function addProducts(array) {
+        array.forEach(el => drawProductCart(el));
+    }
+
+    addProducts(booksList);
 });
